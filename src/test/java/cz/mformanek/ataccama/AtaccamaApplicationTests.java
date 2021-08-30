d0ab3da27232d19e85b2d425c66fd124bd7bb864
Formánek Michal <michal.formanek@generaliceska.cz>
2021-08-30 17:09:35 +0200

    Initial commit

---
+++ src/test/java/cz/mformanek/ataccama/AtaccamaApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.mformanek.ataccama;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtaccamaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
