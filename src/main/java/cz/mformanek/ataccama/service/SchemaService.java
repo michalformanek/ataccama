ef7bd6b06fcfbdf8924796806acaa47581791e23
Formánek Michal <michal.formanek@generaliceska.cz>
2021-08-30 19:24:56 +0200

    Task 2: JDBC template with mapper

--- src/main/java/cz/mformanek/ataccama/service/SchemaService.java
+++ src/main/java/cz/mformanek/ataccama/service/SchemaService.java
@@ -1,13 +1,11 @@
 package cz.mformanek.ataccama.service;
 
+import cz.mformanek.ataccama.mapper.MapMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
-import javax.sql.DataSource;
-import java.sql.ResultSet;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -15,34 +13,14 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class SchemaService {
 
-    private final DataSource dataSource;
+    private final JdbcTemplate jdbcTemplate;
 
     @SneakyThrows
     public List<Map<String, String>> getSchemas() {
-        //We could create repository for this, or call connection.getMetadata().getCatalogs(), which gives less information about schemas
-        final String query = "SELECT * FROM `SCHEMATA`";
-        final var connection = dataSource.getConnection();
-        connection.setCatalog("INFORMATION_SCHEMA");
-        final var result = connection.prepareStatement(query).executeQuery();
-        return map(result);
-    }
-
-    @SneakyThrows
-    private List<Map<String, String>> map(ResultSet rs) {
-        List<Map<String, String>> result = new ArrayList<>();
-        var rsMetaData = rs.getMetaData();
-        var columnCount = rsMetaData.getColumnCount();
-
-        while (rs.next()) {
-            Map<String, String> row = new HashMap<>();
-            for (int i = 1; i <= columnCount; i++) {
-                final String key = rsMetaData.getColumnName(i);
-                final String value = rs.getString(i);
-                row.put(key, value);
-            }
-            result.add(row);
-        }
-        return result;
+        //We could create repository for this, or autowire datasource and connection.getMetadata().getCatalogs()
+        //which gives less information about schemas
+        final String query = "SELECT * FROM INFORMATION_SCHEMA.SCHEMATA";
+        return jdbcTemplate.query(query, new MapMapper());
     }
 
 }
